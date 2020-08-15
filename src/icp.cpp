d66a969034bcb2aefda8abd1a8a658187a36ac61
JI-9-Z <260259120@qq.com>
2020-08-15 16:19:34 +0800

    123

--- src/icp.cpp
+++ src/icp.cpp
@@ -11,12 +11,10 @@
 #include "nav_msgs/Odometry.h"
 #include <tf/transform_broadcaster.h>
 #include <sensor_msgs/LaserScan.h>
-#include <geometry_msgs/Pose.h>
 
 using namespace std;
 using namespace Eigen;
-
-geometry_msgs::Pose pre_pose;
+//lalala
 
 // structure of the nearest neighbor
 typedef struct{
